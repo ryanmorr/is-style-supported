68eeef7dd04788ca5c7e09c62ec6785316bb17d7
Ryan Morr <rm.morr@gmail.com>
2015-05-02 15:05:31 -0400

    Simplify test cases

--- test/test-is-style-supported.js
+++ test/test-is-style-supported.js
@@ -26,39 +26,61 @@ describe('isStyleSupported', function(){
     }
 
     it('should return true for supported properties', function(){
+        var props = [
+            'background',
+            'border',
+            'display',
+            'font-size',
+            'margin',
+            'padding'
+        ];
         testAssertions(function(){
-            expect(isStyleSupported('background')).to.equal(true);
-            expect(isStyleSupported('border')).to.equal(true);
-            expect(isStyleSupported('display')).to.equal(true);
-            expect(isStyleSupported('font-size')).to.equal(true);
-            expect(isStyleSupported('margin')).to.equal(true);
-            expect(isStyleSupported('padding')).to.equal(true);
+            props.forEach(function(prop){
+                expect(isStyleSupported(prop)).to.equal(true);
+            });
         });
     });
 
     it('should return false for unsupported properties', function(){
+        var props = [
+            'foo',
+            'bar',
+            'foo-bar'
+        ];
         testAssertions(function(){
-            expect(isStyleSupported('foo')).to.equal(false);
-            expect(isStyleSupported('bar')).to.equal(false);
-            expect(isStyleSupported('foo-bar')).to.equal(false);
+            props.forEach(function(prop){
+                expect(isStyleSupported(prop)).to.equal(false);
+            });
         });
     });
 
     it('should return true for supported properties and values', function(){
+        var styles = [
+            'display: block',
+            'display: inline',
+            'margin: 5px',
+            'padding: 5em'
+        ];
         testAssertions(function(){
-            expect(isStyleSupported('display', 'block')).to.equal(true);
-            expect(isStyleSupported('display', 'inline')).to.equal(true);
-            expect(isStyleSupported('margin', '5px')).to.equal(true);
-            expect(isStyleSupported('padding', '5em')).to.equal(true);
+            styles.forEach(function(style){
+                var parts = style.split(/\s*:\s*/), prop = parts[0], value = parts[1];
+                expect(isStyleSupported(prop, value)).to.equal(true);
+            });
         });
     });
 
     it('should return false for unsupported properties and values', function(){
+        var styles = [
+            'display: foo',
+            'font-size: bar',
+            'margin: 5foo',
+            'padding: 3bar'
+        ];
         testAssertions(function(){
-            expect(isStyleSupported('display', 'foo')).to.equal(false);
-            expect(isStyleSupported('font-size', 'bar')).to.equal(false);
-            expect(isStyleSupported('margin', '5foo')).to.equal(false);
-            expect(isStyleSupported('padding', '3bar')).to.equal(false);
+            styles.forEach(function(style){
+                var parts = style.split(/\s*:\s*/), prop = parts[0], value = parts[1];
+                expect(isStyleSupported(prop, value)).to.equal(false);
+            });
         });
     });
 
